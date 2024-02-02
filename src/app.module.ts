3f96a0a255083f239a9e1aaeca3932de1c2e0512
Yahya Eddhissa <yahya.eddhissa@proton.me>
2024-02-02 16:22:03 +0000

    app: Set autoLoadEntities to true

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
 import { MessageResolver } from "./message.resolver";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { ProductEntity } from "./product/product.entity";
 import { ProductModule } from "./product/product.module";
 
 const DatabaseModule = TypeOrmModule.forRoot({
@@ -16,7 +15,7 @@ const DatabaseModule = TypeOrmModule.forRoot({
   username: "postgres",
   password: "postgres",
   database: "postgres",
-  entities: [ProductEntity],
+  autoLoadEntities: true,
   synchronize: true,
 });
 
